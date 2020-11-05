d35e1f21a187a27a036ca0035f236dbad53fc3a2
Melisa Budini <melixbq@gmail.com>
2020-11-04 22:51:47 -0300

    paypal

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,21 @@ class OrdersController < ApplicationController
     libro = Libro.find(params[:libro_id])
     order = Order.find_or_create_by(user_id: current_user.id, libro_id: libro.id, paid: false)
     order.quantity += 1
+    if order.save 
+      redirect_to root_path, notice: 'Se ha reservado el libro'
+    else 
+      redirect_to root_path, alert: 'No se ha podido reservar el libro'
+  end  
+  end
+  def destroy
+    order = Order.find(params[:id])
+    order.destroy 
+    redirect_to orders_path, notice: 'Se ha quitado el libro de las reservas'
+  end
+  def clean
+    order = Order.where(paid: false)
+    order.destroy_all
+    redirect_to orders_path, notice: 'Ya no tienes ningún libro reservado'
   end
   
 end
