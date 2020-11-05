092cbaa8c4836dfb155f4f335d4f0e003ef0154b
Melisa Budini <melixbq@gmail.com>
2020-11-04 23:05:02 -0300

    upd paypal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'orders/index'
   root 'libros#index'
+  resources :libros, only: :index do
+    resources :orders, only: :create 
+  end
+  resources :orders, only: :index
   resources :libros
+  delete 'order/:id', to: 'orders#destroy', as: 'destroy_order'
+  
+  resources :billings, only: [] do
+    collection do
+      get 'pre-pay'
+      get 'execute'
+    end 
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
